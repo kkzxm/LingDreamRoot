f8226523e3d0ee8a58513c0cdb1a8280ccee4894
酷酷宅小明 <2499058386@qq.com>
2021-04-29 11:19:39 +0800

    第二版开始, 一. 添加了两个环绕通知类: 1. ControllerBefore.java     改变控制层中传入的值, 2. ServiceBefore.java     改变service层中传入的值,影响到的方法:insert和updateById
    二. 修改了控制层的结构
    1. BaseController.java 作为顶级控制层类
    2. MyController.java 作为存储中间类,存储公共的方法
    3. ControllerImpl.java 作为实现类,真正实现BaseController中的方法

---
+++ src/main/java/com/lingDream/root/controller/ControllerImpl.java
@@ -0,0 +1,63 @@
+package com.lingDream.root.controller;
+
+import com.lingDream.root.service.MyService;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.Serializable;
+
+/**
+ * @Author: 酷酷宅小明
+ * @CreateTime: 2021-04-07 09:34
+ */
+public abstract class ControllerImpl<T> extends MyController<T> {
+    public ControllerImpl(MyService<T> service, String COMMENT) {
+        super(service, COMMENT);
+    }
+
+    @Override
+    public String getPage(Model model, String val, String filter, Integer thisPage) {
+        return setThisPageInData(model, val, filter, thisPage);
+    }
+
+    @Override
+    public String toInsertPage(Model model) {
+        return setToInsertPage(model);
+    }
+
+    @Override
+    public String add(HttpServletRequest request, T entity, Model model) {
+        super.setRequestURL(request, model);
+        model.addAttribute("result", COMMENT + "添加成功");
+        if (!service.insert(entity)) {
+            model.addAttribute("result", COMMENT + "添加失败");
+        }
+        return "admin/adminPublic/addResult";
+    }
+
+    @Override
+    public String delById(HttpServletRequest request, T entity) {
+        if (service.deleteById((Serializable) entity)) {
+            return COMMENT + "删除成功";
+        } else {
+            return COMMENT + "删除失败";
+        }
+    }
+
+    @Override
+    public String updateFindById(HttpServletRequest request, T entity, Model model) {
+        T t = service.selectById((Serializable) entity);
+        model.addAttribute("entity", t);
+        return toInsertPage(model);
+    }
+
+    @Override
+    public String update(HttpServletRequest request, T entity, Model model) {
+        super.setRequestURL(request, model);
+        model.addAttribute("result", COMMENT + "修改成功");
+        if (!service.updateById(entity)) {
+            model.addAttribute("result", COMMENT + "修改失败");
+        }
+        return "admin/adminPublic/addResult";
+    }
+}
--- src/main/java/com/lingDream/root/controller/MyController.java
+++ src/main/java/com/lingDream/root/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.lingDream.root.controller;
 
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.lingDream.root.service.MyService;
 import com.lingDream.root.tool.MyPage;
@@ -8,12 +9,15 @@ import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static com.lingDream.root.utils.strUtil.StringUtils.lowFirstChar;
+import static java.util.Objects.isNull;
+
 
 /**
- * @Author: LI_Lingfei
+ * @Author: 酷酷宅小明
  * @CreateTime: 2021-03-06 14:46
  */
-public abstract class MyController<T> {
+public abstract class MyController<T> implements BaseController<T> {
     protected final MyService<T> service;
     protected final String COMMENT;
 
@@ -22,7 +26,56 @@ public abstract class MyController<T> {
         this.COMMENT = COMMENT;
     }
 
+    //region getMyPage
+    protected MyPage<T> getMyPage(Integer thisPage) {
+        return service.selectPage(new MyPage<>(thisPage, 10), null);
+    }
+
+    protected MyPage<T> getMyPage(Integer thisPage,Integer pageSize) {
+        return service.selectPage(new MyPage<>(thisPage, pageSize), null);
+    }
+
+    protected MyPage<T> getMyPage(Integer thisPage,Wrapper<T> wrapper) {
+        return service.selectPage(new MyPage<>(thisPage, 10), wrapper);
+    }
+    //endregion
+
     //region 设置信息
+
+    //region 公用
+    /**
+     * 设置共用的标题
+     */
+    protected void setTitle(Model model){
+        String substring = COMMENT.substring(0, COMMENT.indexOf(" →"));
+        model.addAttribute("title", substring);
+    }
+    //endregion
+
+    //region 添加或修改相关
+    /**
+     * 设置路径(添加/修改),
+     * 此方法应该放在子类toInsertPage的第一行
+     */
+    protected void setPath(Model model) {
+        String path = model.asMap().size() == 0 ? "add" : "update";
+        model.addAttribute("path", path);
+    }
+
+    //region 去到添加页面
+    public String setToInsertPage(Model model) {
+        setPath(model);
+        setTitle(model);
+
+        String partAddress = this.getClass().getSimpleName();
+        partAddress = partAddress.substring(0, partAddress.length() - 10);
+        model.addAttribute("partAddress", lowFirstChar(partAddress));
+
+        return "admin/index/insert";
+    }
+    //endregion
+
+    //region 添加或修改完成后,指定需要跳转到的页面
     protected void setRequestURL(HttpServletRequest request, Model model) {
         String requestURL = request.getRequestURL().toString();
         String[] split = requestURL.split("/");
@@ -31,33 +84,79 @@ public abstract class MyController<T> {
     }
     //endregion
 
-    //region 得到page
-    public void getMyPage(Integer thisPage, Integer pageSize) {
-        service.selectPage(new MyPage<>(thisPage, pageSize));
-    }
+    //endregion
 
-    public MyPage<T> getMyPage(Integer thisPage) {
-        MyPage<T> myPage = new MyPage<>(thisPage, 10);
-        return service.selectPage(myPage);
-    }
+    //region 查询相关
+    //region 设置当前页面所需的数据
+    /**
+     * 设置数据显示页面需要用的数据,
+     * 如果有模糊查询,
+     *
+     * @param model    模型数据
+     * @param val      模糊查询时的值 可以为null
+     * @param filter   模糊查询数据库表名 可以为null
+     * @param thisPage 当前页
+     * @return 前端显示查询结果的页面路径
+     */
+    protected String setThisPageInData(Model model, String val, String filter, Integer thisPage) {
+        if (thisPage == null) thisPage = 1;
+        Wrapper<T> wrapper = getWrapper(val, filter);
+        MyPage<T> myPage = getMyPage(thisPage, wrapper);
+        model.addAttribute("page", myPage);
 
-    public MyPage<T> getMyPage(MyPage<T> myPage) {
-        return service.selectPage(myPage);
-    }
+        setPageTitleAndPartAddress(model, val, filter);
 
-    public MyPage<T> getMyPage(MyPage<T> myPage, Wrapper<T> wrapper) {
-        return service.selectPage(myPage,wrapper);
+        return "admin/index/list";
     }
+    //endregion
 
-    public MyPage<T> getMyPage(Integer thisPage, Wrapper<T> wrapper) {
-        MyPage<T> myPage = new MyPage<>(thisPage, 10);
-        return service.selectPage(myPage,wrapper);
+    //region 拼接查询时用的wrapper
+    /**
+     * <pre>
+     *      此方法作为拼接条件构造器,
+     * 模糊查询或查询分页时,
+     * 通过登录的班级账号(管理员)信息,
+     * 过滤掉与本班级无关的信息
+     * (如果是超级管理员登录,则无视过滤,查询出全部),
+     *
+     *
+     * </pre>
+     *
+     * @param val     模糊查询时的值
+     * @param filter  糊糊查询用的数据库字段名
+     * @return 拼接出的条件构造器
+     */
+    private Wrapper<T> getWrapper(String val, String filter) {
+        Wrapper<T> wrapper = new EntityWrapper<>();
+        wrapper.like(filter, val);
+        return wrapper;
     }
+    //endregion
+    //endregion
 
-    public MyPage<T> getMyPage(Integer thisPage,Integer pageSize, Wrapper<T> wrapper) {
-        MyPage<T> myPage = new MyPage<>(thisPage, pageSize);
-        return service.selectPage(myPage,wrapper);
+    //region 设置零件地址
+    /**
+     * 反射获取小写类名,去除Controller字符串(10个字符)
+     * 用来设置
+     * 页面零件地址
+     */
+    private void setPageTitleAndPartAddress(Model model, String val, String filter) {
+        setTitle(model);
+
+        String partAddress = this.getClass().getSimpleName();
+        partAddress = lowFirstChar(partAddress.substring(0, partAddress.length() - 10));
+        model.addAttribute("partAddress", partAddress);
+
+        /*
+        分页时用到的地址
+        */
+        String limitPath = partAddress + "/getPage?";
+        if (!isNull(val)) limitPath += "val=" + val + "&";
+        if (!isNull(filter)) limitPath += "filter=" + filter + "&";
+        limitPath += "thisPage";
+        model.addAttribute("limitPath", limitPath);
     }
+    //endregion
 
     //endregion
 }
